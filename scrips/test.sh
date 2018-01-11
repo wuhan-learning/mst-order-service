fc441e52481503cded028a8a79fd58a403bda69b
hbhuang <bhhuang@thoughtworks.com>
2018-01-11 20:19:16 +0800

    Add scrips folder

---
+++ scrips/test.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+set -x 
+set -e
+
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GOPIPELINENAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean test scripts/build.sh
