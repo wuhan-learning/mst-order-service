5e6c64243dc74b1f422b2cf87f8536b938c114d6
hbhuang <bhhuang@thoughtworks.com>
2018-01-15 18:16:58 +0800

    Remove used text in test.sh

--- scrips/test.sh
+++ scrips/test.sh
@@ -2,4 +2,4 @@
 set -x 
 set -e
 
-docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GOPIPELINENAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean test scripts/build.sh
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GOPIPELINENAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean test
