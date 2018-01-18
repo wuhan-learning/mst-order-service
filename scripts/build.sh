e46ce68c2f8f73cce074a20e881bc88c03f17f16
hbhuang <bhhuang@thoughtworks.com>
2018-01-18 13:36:30 +0800

    Add scripts folder and docker-compose and rancher-compose

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -x
+set -e
+
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean bootRepackage
+if [[ -z $DOCKER_REGISRTY ]]; then
+  DOCKER_REGISRTY=10.202.129.3:5000
+fi
+IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-train/order-service:${GO_PIPELINE_COUNTER}
+docker build -t $IMAGE_NAME .
+docker push $IMAGE_NAME
+docker rmi $IMAGE_NAME
