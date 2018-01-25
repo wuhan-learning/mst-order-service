2e5e6cc0047897284c09fe86131e024deed73c6d
YuanShenjian <sjyuan@thoughtworks.com>
2018-01-25 08:41:09 +0800

    Fix deploy script

--- scrips/deploy.sh
+++ scrips/deploy.sh
@@ -2,9 +2,15 @@
 set -x
 set -e
 
+export RANCHER_URL=http://10.202.129.3:8080/v2-beta/projects/1a5
+export RANCHER_ACCESS_KEY=0776A1C81D57800F4CE9
+export RANCHER_SECRET_KEY=Z2i8KcmfzeroaAy148wuPnxjyhwGxmxm3qZWsZC8
+
 if [[ -z $DOCKER_REGISRTY ]]; then
   DOCKER_REGISRTY=10.202.129.3:5000
 fi
 IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-training/order-service:${GO_PIPELINE_COUNTER}
 
-docker run -d -p 8080:8080 $IMAGE_NAME
+sed -i "s#<IMAGE_NAME>#$IMAGE_NAME#g" docker-compose.yml
+
+rancher-compose -p mst-user-service up -d -c --upgrade
