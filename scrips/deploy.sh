c62db12d16c44a2b0c1865240f47c12b767a03bb
YuanShenjian <sjyuan@thoughtworks.com>
2018-01-25 08:40:19 +0800

    Config to deploy

--- scrips/deploy.sh
+++ scrips/deploy.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 
 if [[ -z $DOCKER_REGISRTY ]]; then
-  DOCKER_REGISRTY=127.0.0.1:5000
+  DOCKER_REGISRTY=10.202.129.3:5000
 fi
-IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-train/user-service:${GO_PIPELINE_COUNTER}
+IMAGE_NAME=${DOCKER_REGISRTY}/tw-ms-training/order-service:${GO_PIPELINE_COUNTER}
 
 docker run -d -p 8080:8080 $IMAGE_NAME
